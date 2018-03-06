096ce59036457770096e3d32d1fceb166664e57d
akhil1102 <akhilb1102@gmail.com>
2018-03-05 20:42:51 -0800

    edit change

--- notes/models.py
+++ notes/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-
 # Create your models here.
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200,null=True)
     body = models.TextField()
+
+    def get_absolute_url(self):
+        return "/notes/detail/%s/" % (self.id)
 
 
 
